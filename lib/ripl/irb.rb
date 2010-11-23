6e405f182d1d2c5d9c9b2c56739e891373a7f5e1
Gabriel Horner <gabriel.horner@gmail.com>
2010-11-22 21:59:17 -0500

    detect and suggest ripl equivalents for IRB.conf

--- lib/ripl/irb.rb
+++ lib/ripl/irb.rb
@@ -1,3 +1,5 @@
+require 'ripl'
+
 module Ripl
   module Irb
     VERSION = '0.1.2'
@@ -7,9 +9,30 @@ module Ripl
       super
     end
 
+    CONFIG_MAP = {
+      :PROMPT_MODE => :prompt, :PROMPT => :prompt, :HISTORY_FILE => :history,
+      :USE_READLINE => :readline, :AP_NAME => :name, :RC => :irbrc
+    }
+    DESC_MAP = {
+      :SINGLE_IRB => "jump commands in ripl-commands plugin have this enabled by default",
+      :IRB_RC => "Use ripl-after_rc plugin instead of IRB.conf[:IRB_RC]",
+      :RC_NAME_GENERATOR => "Use Ripl.config[:history] or Ripl.config[:irbrc] "+
+        "instead of IRB.conf[:RC_NAME_GENERATOR]",
+      :LOAD_MODULES => 'No need for irb or ripl to do this. Just use require :)'
+    }
+
     def mock_irb
       mod = Object.const_set(:IRB, Module.new).extend(MockIrb)
-      class <<mod; self end.send(:define_method, :conf) { {} }
+      conf = {}
+      def conf.[]=(key, val)
+        return unless Ripl.config[:irb_verbose]
+        if ripl_key = CONFIG_MAP[key]
+          puts "Use Ripl.config[#{ripl_key.inspect}] instead of IRB.conf[#{key.inspect}]"
+        elsif desc = DESC_MAP[key]
+          puts desc
+        end
+      end
+      class <<mod; self end.send(:define_method, :conf) { conf }
     end
 
     module MockIrb
@@ -23,4 +46,5 @@ module Ripl
   end
 end
 
-Ripl::Shell.send :include, Ripl::Irb if defined? Ripl::Shell
+Ripl::Shell.send :include, Ripl::Irb
+Ripl.config[:irb_verbose] = true
